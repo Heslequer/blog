0dea140e7507a1292399b37d53a5f34f3a67b290
Heslequer <hesleiquer5@gmail.com>
2024-09-29 17:03:01 -0300

    Atualizando article

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,10 +1,20 @@
 import Post from '../Post'
 function Article(){
     return(
-        <>
-            <Post></Post>
-            <h1>Article</h1>
-        </>
+        <section className='container'>
+            <h3>Article</h3>
+            <div className='mt-3 container-posts'>
+            <Post subtitle='tecnologia' title='O gui definitivo sobre o blog'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellantesque. Utricies habitasse pretium purus viverra. Sit eget volutpat semper vitae metus,ringilla ullamcorper sapien nibh.  
+            </  Post>
+            <Post subtitle='fotografia' title='Quais as melhores câmeras em 2021'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellantesque. Utricies habitasse pretium purus viverra. Sit eget volutpat semper vitae metus,ringilla ullamcorper sapien nibh.
+            </Post>
+            <Post subtitle='cinema' title='Os 10 filmes com maior bilheteria'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellantesque. Utricies habitasse pretium purus viverra. Sit eget volutpat semper vitae metus,ringilla ullamcorper sapien nibh.
+            </Post>
+            </div>
+        </section>
        
     );
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,6 +1,20 @@
-function Post(){
+function Post(props){
     return(
-        <h1>Post</h1>
+        <div className="post mx-2">
+            <div className="img-post"></div>
+            <h5>{props.subtitle}</h5>
+            <h3>{props.title}</h3>
+            <p>
+                {props.children}
+            </p>
+            <div className=" flex pt-1">
+                <div className="img-profile"></div>
+                <div className="desc-profile ml-2 mt-1">
+                    <h6 className="color-blue"> Fulano de tal</h6>
+                    <p>Aug 2, 2020 - 8 min read</p>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Post
